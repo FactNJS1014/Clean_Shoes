463800ed1da02b565508a3cca78a6781f3c6132e
FactOK <147402799+FactNJS1014@users.noreply.github.com>
2025-07-22 09:30:23 +0700

    delete file and rename title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ESD Check Setting</title>
+    <title>Report Cleaning Shoes</title>
     <link rel="shortcut icon" href="{{asset('/public/images/ESD Setting v2.svg')}}" type="image/x-icon">
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
